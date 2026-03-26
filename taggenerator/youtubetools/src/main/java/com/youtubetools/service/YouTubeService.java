3f5b7b41e5e18dc62d0dbbe097964a3c7e44ddac
abhishek-gehlot <abhishekgehlot012@gmail.com>
2026-03-26 13:46:56 +0530

    Add files via upload

---
+++ taggenerator/youtubetools/src/main/java/com/youtubetools/service/YouTubeService.java
@@ -0,0 +1,16 @@
+package com.youtubetools.service;
+
+
+import org.springframework.stereotype.Service;
+
+import com.youtubetools.model.SearchVideo;
+
+@Service
+public class YouTubeService {
+
+    public SearchVideo searchVideos(String videoTitle) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchVideos'");
+    }
+    
+}
